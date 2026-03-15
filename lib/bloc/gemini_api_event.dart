12718a39c73b5737966c039b08446013ac799123
Yianday <yianday0330h@gmail.com>
2026-03-16 00:03:41 +0800

    feat: Implement multimodal input for Gemini API, allowing users to send images with text prompts.

--- lib/bloc/gemini_api_event.dart
+++ lib/bloc/gemini_api_event.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:equatable/equatable.dart';
 
 abstract class GeminiApiEvent extends Equatable {}
@@ -9,9 +11,11 @@ class InitEvent extends GeminiApiEvent {
 
 class QueryEvent extends GeminiApiEvent {
   final String query;
+  final Uint8List? imageBytes;
+  final String? mimeType;
 
-  QueryEvent({required this.query});
+  QueryEvent({required this.query, this.imageBytes, this.mimeType});
 
   @override
-  List<Object?> get props => [query];
+  List<Object?> get props => [query, imageBytes, mimeType];
 }
