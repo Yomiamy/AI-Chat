a8db9d182a5ee1ef26e2a20a50455615e74e81a1
Yianday <yianday0330h@gmail.com>
2026-03-28 02:11:16 +0800

    Remove the Gemini API bloc implementation

--- lib/pages/ai_chat_page.dart
+++ lib/pages/ai_chat_page.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:ai_chat/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
