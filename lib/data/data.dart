574bf0ee91c64621e65e5bc3c496b355bd747dc6
Yianday <yianday0330h@gmail.com>
2026-05-21 02:12:30 +0800

    refactor: keep data exports clean by only exporting interface and entity

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,2 +1,3 @@
 export 'chat_message.dart';
 export 'chat_repository.dart';
+// Note: Do NOT export chat_repository_impl.dart here to prevent ObjectBox native library dependency from leaking into unit tests.
